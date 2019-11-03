2fc6f30487e5c5ce7fb107ba99b9575d53f05544
Jesus Aguas <736935@unizar.es>
2019-11-03 02:29:05 -0800

    Camera added

---
+++ SafeCam/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SafeCam
+//
+//  Created by Jesús Aguas Acin on 02/11/2019.
+//  Copyright © 2019 Jesus Aguas Acin. All rights reserved.
+//
+
+import UIKit
+
+// Put this piece of code anywhere you like
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.hideKeyboardWhenTappedAround()
+    }
+
+
+}
